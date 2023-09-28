e20615dec9b38acba56beb0f07ed8f9d9ef8bcf1
AleksandrA010 <dark_letsplay@mail.ru>
2023-09-28 21:38:35 +0300

    Add project files.

---
+++ Lab_work/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+
+namespace Lab_work
+{
+    internal class Program
+    {
+        static string Repl(string str)
+        {
+            str = str.Replace("y", "Y");
+            str = str.Replace("n", "N");
+            return str;
+        }
+        static bool Year_check(int year)
+        {
+            bool result;
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            else
+            {
+                if (year % 4 == 0)
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+        enum Months
+        {
+            January = 1,
+            February = 2,
+            March = 3,
+            April = 4,
+            May = 5,
+            June = 6,
+            July = 7,
+            August = 8,
+            September = 9,
+            October = 10,
+            November = 11,
+            December = 12,
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Так как задания почти идентичны, то для удобства они были объеденены в одно.\n");
+            Console.WriteLine("Задание 1 / (1,2,3).\nПограмма принимает число(1, 365) и переводит в месяц и день месяца. Обработать возможные ошибки и високосный год.\n");
+            Console.Write("Желаете ли вы проверить задание 1 (Yes, No): ");
+            string answer = Console.ReadLine();
+            answer = Repl(answer);
+            bool flag = true;
+            int[] days = { 28, 30, 31 };
+            while (flag)
+            { 
+                switch (answer)
+                {
+                    case "Yes":
+                        Console.Write("\nВведите год(н.э.): ");
+                        bool check1 = int.TryParse(Console.ReadLine(), out int year);
+                        if (!check1 | year <= 0)
+                        {
+                            Console.Write("\nВы не правильно ввели данные, хотите попробовать ещё раз: ");
+                            answer = Console.ReadLine();
+                            answer = Repl(answer);
+                            continue;
+                        }
+                        bool leap_year = Year_check(year);
+                        Console.Write("Введите номер дня в году (Натуральное число от 1 до 365/366, если високосный год): ");
+                        bool check2 = int.TryParse(Console.ReadLine(), out int day);
+                        if (!check2 | day <= 0)
+                        {
+                            Console.Write("\nВы не правильно ввели данные, хотите попробовать ещё раз: ");
+                            answer = Console.ReadLine();
+                            answer = Repl(answer);
+                            continue;
+                        }
+                        else
+                        {
+                            if (day > 366 | day < 1)
+                            {
+                                Console.Write("\nВы не правильно ввели данные, хотите попробовать ещё раз:  ");
+                                answer = Console.ReadLine();
+                                answer = Repl(answer);
+                                continue;
+                            }
+                            else if (day == 366 & leap_year == false)
+                            {
+                                Console.Write("\nДанный год не является високосным, данные не корректны, хотите попробовать ещё раз: ");
+                                answer = Console.ReadLine();
+                                answer = Repl(answer);
+                                continue;
+                            }
+                        }
+                        int? result_month = null;
+                        int? result_day = null;
+                        if (!leap_year)
+                        {
+                            if (1 <= day & day <= 31)
+                            {
+                                result_month = 1;
+                                result_day = day;
+                            }
+                            else if (32 <= day & day <= 59)
+                            {
+                                result_month = 2;
+                                result_day = day - days[2];
+                            }
+                            else if (60 <= day & day <= 90)
+                            {
+                                result_month = 3;
+                                result_day = day - days[0] - days[2];
+                            }
+                            else if (91 <= day & day <= 120)
+                            {
+                                result_month = 4;
+                                result_day = day - days[0] - days[2] - days[1];
+                            }
+                            else if (121 <= day & day <= 151)
+                            {
+                                result_month = 5;
+                                result_day = day - days[0] - 2 * days[2] - days[1];
+                            }
+                            else if (152 <= day & day <= 181)
+                            {
+                                result_month = 6;
+                                result_day = day - days[0] - 2 * days[2] - 2 * days[1];
+                            }
+                            else if (182 <= day & day <= 212)
+                            {
+                                result_month = 7;
+                                result_day = day - days[0] - 3 * days[2] - 2 * days[1];
+                            }
+                            else if (213 <= day & day <= 243)
+                            {
+                                result_month = 8;
+                                result_day = day - days[0] - 4 * days[2] - 2 * days[1];
+                            }
+                            else if (244 <= day & day <= 273)
+                            {
+                                result_month = 9;
+                                result_day = day - days[0] - 4 * days[2] - 3 * days[1];
+                            }
+                            else if (274 <= day & day <= 304)
+                            {
+                                result_month = 10;
+                                result_day = day - days[0] - 5 * days[2] - 3 * days[1];
+                            }
+                            else if (305 <= day & day <= 334)
+                            {
+                                result_month = 11;
+                                result_day = day - days[0] - 5 * days[2] - 4 * days[1];
+                            }
+                            else if (335 <= day & day <= 365)
+                            {
+                                result_month = 12;
+                                result_day = day - days[0] - 6 * days[2] - 4 * days[1];
+                            }
+
+                        }
+                        else
+                        {
+                            if (1 <= day & day <= 31)
+                            {
+                                result_month = 1;
+                                result_day = day;
+                            }
+                            else if (32 <= day & day <= 60)
+                            {
+                                result_month = 2;
+                                result_day = day - days[2];
+                            }
+                            else if (61 <= day & day <= 91)
+                            {
+                                result_month = 3;
+                                result_day = day - days[0] - days[2] - 1;
+                            }
+                            else if (92 <= day & day <= 121)
+                            {
+                                result_month = 4;
+                                result_day = day - days[0] - days[2] - days[1] - 1;
+                            }
+                            else if (122 <= day & day <= 152)
+                            {
+                                result_month = 5;
+                                result_day = day - days[0] - 2 * days[2] - days[1] - 1;
+                            }
+                            else if (153 <= day & day <= 182)
+                            {
+                                result_month = 6;
+                                result_day = day - days[0] - 2 * days[2] - 2 * days[1] - 1;
+                            }
+                            else if (183 <= day & day <= 213)
+                            {
+                                result_month = 7;
+                                result_day = day - days[0] - 3 * days[2] - 2 * days[1] - 1;
+                            }
+                            else if (214 <= day & day <= 244)
+                            {
+                                result_month = 8;
+                                result_day = day - days[0] - 4 * days[2] - 2 * days[1] - 1;
+                            }
+                            else if (245 <= day & day <= 274)
+                            {
+                                result_month = 9;
+                                result_day = day - days[0] - 4 * days[2] - 3 * days[1] - 1;
+                            }
+                            else if (275 <= day & day <= 305)
+                            {
+                                result_month = 10;
+                                result_day = day - days[0] - 5 * days[2] - 3 * days[1] - 1;
+                            }
+                            else if (306 <= day & day <= 335)
+                            {
+                                result_month = 11;
+                                result_day = day - days[0] - 5 * days[2] - 4 * days[1] - 1;
+                            }
+                            else if (336 <= day & day <= 366)
+                            {
+                                result_month = 12;
+                                result_day = day - days[0] - 6 * days[2] - 4 * days[1] - 1;
+                            }
+                        }
+
+                        Console.WriteLine($"Year: {year};\nMonth: {(Months)result_month};\nDay: {result_day}.");
+                        Console.Write("\nЖелаете ли вы ещё раз проверить задание 1 (Yes, No): ");
+                        answer = Console.ReadLine();
+                        answer = Repl(answer);
+                        break;
+                    case "No":
+                        flag = false;
+                        break;
+                    default:
+                        Console.Write("Введите Yes/No: ");
+                        answer = Console.ReadLine();
+                        answer = Repl(answer);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Tasks/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace Tasks
+{
+    enum Days_Week
+    {
+        Понедельник = 1,
+        Вторник = 2,
+        Среда = 3,
+        Четверг = 4,
+        Пятница = 5,
+        Суббота = 6,
+        Воскресенье = 7,
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Доступные для проверки задания (1,2,3,4,5), для проверки введите его номер.\n");
+            Console.WriteLine("Для выхода из программы введите break.\n");
+            Console.WriteLine("Задание 1.\n");
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("\nВведите номер задания для проверки: ");
+                string number = Console.ReadLine();
+                switch (number)
+                {
+                    case "1":
+                        Console.Write("\nВведите последовательность из 10 чисел (Последовательность должна быть целой, не отрицательной): ");
+                        bool check = int.TryParse(Console.ReadLine(), out int sequence);
+                        if (!check | (sequence >= 10000000000) | (sequence < 1000000000))
+                        {
+                            Console.WriteLine("Вы не верно ввели данные.");
+                            continue;
+                        }
+                        int number1, number2;
+                        int? last_el = null;
+                        int? last_el_index = null;
+                        bool flag_sequence = true;
+                        for (int i = 1; i < 10; i++)
+                        {
+                            sequence /= 10;
+                            number1 = sequence % 10;
+                            number2 = sequence % 100 / 10;
+                            if (number1 < number2)
+                            {
+                                last_el_index = 10 - i;
+                                last_el = number1;
+                                flag_sequence = false;
+                            }
+
+                        }
+                        if (flag_sequence)
+                        {
+                            Console.WriteLine("Последовательность — возрастающая.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Последовательность — не возрастающая, счёт остановился на {last_el} — это {last_el_index} элемент в последовательности.");
+                        }
+                            break;
+                    case "2":
+                        Console.Write("\nВведите порядковый номер карты (число от 6 до 14): ");
+                        int number_card;
+                        try
+                        {
+                            number_card = int.Parse(Console.ReadLine());
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Вы не верно ввели данные.");
+                            continue;
+                        }
+                        switch (number_card)
+                        {
+                            case 6:
+                                Console.WriteLine("Это шестёрка.");
+                                break;
+                            case 7:
+                                Console.WriteLine("Это семёрка.");
+                                break;
+                            case 8:
+                                Console.WriteLine("Это восьмёрка.");
+                                break;
+                            case 9:
+                                Console.WriteLine("Это девятка.");
+                                break;
+                            case 10:
+                                Console.WriteLine("Это десятка.");
+                                break;
+                            case 11:
+                                Console.WriteLine("Это валет.");
+                                break;
+                            case 12:
+                                Console.WriteLine("Это дама.");
+                                break;
+                            case 13:
+                                Console.WriteLine("Это король.");
+                                break;
+                            case 14:
+                                Console.WriteLine("Это туз.");
+                                break;
+                            default:
+                                Console.WriteLine("Вы не верно ввели данные.");
+                                break;
+                        }
+                        break;
+                    case "3":
+                        Console.Write("Введите что-либо из предложенного — Jabroni, School Counselor, Programmer,\nBike Gang Member, Politician, Rapper: ");
+                        string str = Console.ReadLine();
+                        str = str.ToLower();
+                        switch (str)
+                        {
+                            case "jabroni":
+                                Console.WriteLine("Patron Tequilla");
+                                break;
+                            case "school counselor":
+                                Console.WriteLine("Anything with Alcohol");
+                                break;
+                            case "programmer":
+                                Console.WriteLine("Hipster Craft Beer");
+                                break;
+                            case "bike gang member":
+                                Console.WriteLine("Moonshine");
+                                break;
+                            case "politician":
+                                Console.WriteLine("Your tax dollars");
+                                break;
+                            case "rapper":
+                                Console.WriteLine("Cristal");
+                                break;
+                            default:
+                                Console.WriteLine("Beer");
+                                break;
+                        }
+                        break;
+                    case "4":
+                        Console.Write("\nВведите порядковый номер дня недели (от 1 до 7): ");
+                        bool check3 = int.TryParse(Console.ReadLine(), out int day_w);
+                        if (!check3 | day_w > 7 | day_w < 1)
+                        {
+                            Console.WriteLine("Вы не верно ввели данные.");
+                            continue;
+                        }
+                        Console.WriteLine($"Это {(Days_Week)day_w}.");
+                        break;
+                    case "5":
+                        break;
+                    case "break":
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("\nВы не верно ввели номер задания.\nДля проверки доступны(1,2,3,4,5); Для выхода из программы введите break.");
+                        break;
+                }
+            }
+        }
+    }
+}
